e1f8222379566136030c77d11cdd6652ad5fb64a
Henrike Schwenn <henrike.schwenn@gmail.com>
2026-09-24 11:14:42 +0200

    Doppelte Dateien gelöscht und Sortieren mit Copy Paste resetted, was ganz, ganz bestimmt schon viele professionelle Entwickler vor mir getan haben

--- javamodul/src/Selbsttestaufgaben1/Sortieren.java
+++ javamodul/src/Selbsttestaufgaben1/Sortieren.java
@@ -3,16 +3,14 @@ package Selbsttestaufgaben1;
 public class Sortieren {
     public Sortieren() {
     }
+
     public static void main(String[] args) {
         double[] feld = new double[args.length];
-        try {
-            for(int i = 0; i < args.length; ++i) {
-                feld[i] = Double.parseDouble(args[i]);
-            }
-        } catch (NumberFormatException e) {
-                System.out.println("Min. ein Argument ist keine Fließkommazahl.");
-            }
+
+        for(int i = 0; i < args.length; ++i) {
+            feld[i] = Double.parseDouble(args[i]);
         }
+
         for(int i = 0; i < args.length - 1; ++i) {
             for(int j = 0; j < args.length - 1 - i; ++j) {
                 if (feld[j] < feld[j + 1]) {
@@ -22,9 +20,12 @@ public class Sortieren {
                 }
             }
         }
+
         System.out.println("Das größte Element ist " + feld[0] + ".");
+
         for(int i = 0; i < args.length; ++i) {
             System.out.println(i + ". " + feld[i]);
         }
+
     }
 }
